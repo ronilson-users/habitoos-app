73b74b9b705b5038979a143a3927dbfc7d4ad7f5
ronilson-user <ronilson.stos@gmail.com>
2023-11-01 01:56:33 -0300

    primeiro commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,10 @@
-import React from 'react'
+/** @format */
 
-
-
+import React from 'react';
+import {} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Navigation from '../../routes/Navigation'
-export default function HomeIndex(){
-	return(
-		  
-		  
-		  <Navigation  />
-		  
-		  
-		)
+import Navigation from '../../routes/Navigation';
+export default function HomeIndex() {
+ return <Navigation />;
 }
