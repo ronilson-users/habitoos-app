6a4169441ec92b5958829d06f3172f84dbcb26c8
ronilson-user <ronilson.stos@gmail.com>
2023-11-01 01:53:21 -0300

    correcao nabigation

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text } from 'react-native'
+
 
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,7 +9,7 @@ export default function HomeIndex(){
 	return(
 		  
 		  
-		  <Navigation />
+		  <Navigation  />
 		  
 		  
 		)
