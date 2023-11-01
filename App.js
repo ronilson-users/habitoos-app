ece563a5178d7502c4567e46be5085f4392d1ff4
ronilson-user <ronilson.stos@gmail.com>
2023-10-31 21:39:16 -0300

    correcoes no cadastco

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js Ronilson S  </Text>
+      <Text>Open up App.js Ronilson Santos  </Text>
       <StatusBar style="auto" />
     </View>
   );
